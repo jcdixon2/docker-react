a0a04a2281c250f8d9783bba2f64fae751d8d1d2
jcdixon <Jack.Dixon_ii@nokia.com>
2020-01-31 22:37:08 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          At least docker-compose works with volume mapping.  Still need the cmd line -v option.
         </p>
         <a
           className="App-link"
--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,13 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react link 1st', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+test('renders learn react link 2nd again', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
